919ad74bfe0ba3e401b08ec9469901bb54084253
TFuray <pk.master881@gmail.com>
2022-08-14 21:10:59 -0700

    solution

--- 5-calculateAverage.js
+++ 5-calculateAverage.js
@@ -5,10 +5,21 @@
 let find1 = [1,1,1];
 let find2 = [1,2,3];
 let find3 = [1,2,3,4];
+let find4 = [];
 
 
 
 function find_average(array) {
     // your code here
-    return 0;
+    let sum = 0;
+    for(let i = 0; i < array.length; i ++) {
+        sum += array[i];
+    } average = sum / array.length;
+    if (isNaN(average)){
+        return 0;
+    }else{
+        return average;
+    }
 }
+
+console.log(find_average(find4));
