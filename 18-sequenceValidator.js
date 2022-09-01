299f7d5cdf558be629046b14041d149e788af16a
TFuray <pk.master881@gmail.com>
2022-09-01 01:30:38 -0700

    completed

--- 18-sequenceValidator.js
+++ 18-sequenceValidator.js
@@ -6,7 +6,7 @@
 
 // JavaScript examples:
 
-// validateSequence([1, 2, 3, 4, 5, 6, 7, 8, 9]) === true
+// let test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 34, 22, 10]
 // validateSequence([1, 2, 3, 4, 5, 8, 7, 8, 9]) === false
 // validateSequence([2, 4, 6, 8, 10]) === true
 // validateSequence([0, 2, 4, 6, 8]) === true
@@ -14,7 +14,19 @@
 // validateSequence([1, 2, 4, 8, 16, 32, 64]) === false
 // validateSequence([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]) === false
 
+let sample = [1, 2, 3, 4, 5, 9, 32]
+
+function check(array){
+    let newOnes = []
+    for(let i = 1; i<array.length; i++){
+        newOnes.push(array[i] - array[i-1])
+    }
+    return newOnes;
+}
+
+const allEqual = arr => arr.every(v => v === arr[0]);
 
 function validateSequence(x) {
-
+    let test = check(x);
+    return allEqual(test)   
 }
