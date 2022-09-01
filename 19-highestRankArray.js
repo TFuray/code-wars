1e7aafb07f95d71153706ec0484e1d8a99c32e14
TFuray <pk.master881@gmail.com>
2022-09-01 12:45:23 -0700

    added challenge

---
+++ 19-highestRankArray.js
@@ -0,0 +1,18 @@
+// Complete the method which returns the number which is most frequent in the given input array.If there
+//  is a tie for most frequent number, return the largest number among them.
+
+//     Note: no empty arrays will be given.
+
+//         Examples
+//     [12, 10, 8, 12, 7, 6, 4, 10, 12]-- > 12
+//     [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]-- > 12
+//     [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]-- > 3
+const test1 = [12, 10, 8, 12, 7, 6, 4, 10, 12]
+const test2 = [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]
+
+
+function highestRank(arr) {
+    
+}
+
+console.log(highestRank(test1));
