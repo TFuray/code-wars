26b7f134d3f6b5afcfcf6e294e8c66d534ce8968
TFuray <pk.master881@gmail.com>
2022-09-11 17:36:51 -0700

    solution

--- 22-multiplesOf3and5.js
+++ 22-multiplesOf3and5.js
@@ -6,5 +6,12 @@
 
 
 function solution(number) {
-
+    let sum = 0
+    for(let i = 1; i < number; i++){
+        if(i % 5 === 0 || i % 3 == 0){
+            sum = i + sum
+        }
+    }return sum
 }
+
+console.log(solution(10));
