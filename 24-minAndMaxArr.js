276fa3e07365675e981938d1461e905933a48034
TFuray <pk.master881@gmail.com>
2022-09-25 15:19:52 -0700

    added missing files

---
+++ 24-minAndMaxArr.js
@@ -0,0 +1,20 @@
+// Task
+// Write a function that returns both the minimum and maximum number of the given list / array.
+
+//     Examples(Input-- > Output)
+// [1, 2, 3, 4, 5]-- > [1, 5]
+// [2334454, 5]-- > [5, 2334454]
+// [1]-- > [1, 1]
+// Remarks
+// All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+
+function minMax(arr) {
+   let a = arr.sort((a,b) => a-b)
+    let aa = a[0]
+    let bb = a[a.length-1]
+    let final = [aa, bb]
+    return final
+}
+
+minMax([1, 2, 3, 4])
