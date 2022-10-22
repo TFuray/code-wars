23d67d1bf1f956464ab566c8f350d855f29cd894
TFuray <pk.master881@gmail.com>
2022-10-21 23:07:32 -0700

    completed

--- 37-plusOneArray.js
+++ 37-plusOneArray.js
@@ -10,3 +10,27 @@ For example the array [2, 3, 9] equals 239, adding one would return the array [2
 
 [4, 3, 2, 5] would return [4, 3, 2, 6]
 */ 
+
+let test = [4, 3, 2, 5]
+
+
+function upArray(arr){
+  for(let i=0; i<arr.length;i++){
+    if(arr[i] < 0 || arr[i] > 9 || arr === []){
+      return null
+    }
+  }
+  let str = ''
+  let endArr = []
+  arr.forEach(element => {
+    str = str + element
+  })
+  str = Number(str) + 1
+  str = str.toString()
+  for(let i=0; i<str.length;i++){
+    endArr.push(Number(str[i]))
+  }
+  return endArr
+}
+
+console.log(upArray(test))
