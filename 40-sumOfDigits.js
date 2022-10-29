588c17e780c3264c9e1c7d19765291ba01c5576f
TFuray <pk.master881@gmail.com>
2022-10-28 19:49:04 -0700

    done

--- 40-sumOfDigits.js
+++ 40-sumOfDigits.js
@@ -7,3 +7,22 @@
 //    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 // 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 // 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+
+let test = 493193
+
+function digitalRoot(n) {
+  let next = Array.from(String(n))
+  let arr = next.map(str => Number(str))
+  let final = 0
+  arr.forEach(element =>{
+    final += element
+  })
+  if(final.length === 1){
+    return final
+  }else{
+    digitalRoot(final)
+  }
+}
+
+ console.log(digitalRoot(test))
