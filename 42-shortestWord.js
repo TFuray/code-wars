33982c12fd6a566664f40696c724a1e31712b7fc
TFuray <pk.master881@gmail.com>
2022-10-31 13:23:14 -0700

    completed

--- 42-shortestWord.js
+++ 42-shortestWord.js
@@ -2,7 +2,15 @@
 
 // String will never be empty and you do not need to account for different data types.
 
-
+let test = 'This is the test will will use.'
 
 function findShort(s){
+  let arr = s.split(' ')
+  let arrLength = arr.map(word => {
+    return word.length
+  })
+  arrLength.sort((a,b) => a-b)
+  return arrLength[0]
 }
+
+console.log(findShort(test))
