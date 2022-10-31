61ab2d5489b84e9de1ba3b3b10ee929ddaed92f7
TFuray <pk.master881@gmail.com>
2022-10-30 17:45:43 -0700

    completed

--- 41-findParityOutlier.js
+++ 41-findParityOutlier.js
@@ -7,9 +7,32 @@
 // [160, 3, 1719, 19, 11, 13, -21]
 // Should return: 160 (the only even number)
 
-
-
+let test = [2, 4, 0, 100, 4, 11, 2602, 36]
+let test2 = [160, 3, 1719, 19, 11, 13, -21]
 
 function findOutlier(integers) {
-  //your code here
+  if (
+    integers[0] % 2 === 0 &&
+    (integers[1] % 2 === 0) & (integers[2] % 2 === 0)
+  ) {
+    for (let i = 0; i < integers.length; i++) {
+      if (integers[i] % 2 !== 0 && integers[i] != 0) {
+        return integers[i]
+      }
+    }
+  } else {
+    for (let i = 0; i < integers.length; i++) {
+      if (integers[i] % 2 == 0 && integers[i] != 0) {
+        return integers[i]
+      }
+    }
+  }
 }
+
+
+
+console.log(findOutlier([0, 1, 2]), 1)
+console.log(findOutlier([1, 2, 3]), 2)
+console.log(findOutlier([0, 0, 3, 0, 0]), 3)
+console.log(findOutlier([1, 1, 0, 1, 1]), 0)
+console.log(findOutlier([2, 6, 8, 10, 3]), 3)
