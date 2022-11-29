8d172ba5a1a4cb25cb28b2e1da9770b8c820b836
TFuray <pk.master881@gmail.com>
2022-11-28 18:55:43 -0800

    completed kata, fun problem

--- 53-WhoLikesIt.js
+++ 53-WhoLikesIt.js
@@ -13,13 +13,21 @@ let test = ["Alex", "Jacob", "Mark", "Max"]
 
 
 function likes (names) {
+  let num = names.length - 2
   if(names.length === 0){
     return 'no one likes this'
   }else if(names.length === 1){
     return `${names[0]} likes this`
-  }else if(names.length === 2){
+  }else if(names.length === 2){    
     return `${names[0]} and ${names[1]} like this`
   }else if(names.length === 3){
-    return `${names[0]} and ${names[1]} like this`
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  }else if(names.length >= 4){
+    return `${names[0]}, ${names[1]} and ${num} others like this`
   }
 }
+
+
+console.log(likes(test))
+
+
