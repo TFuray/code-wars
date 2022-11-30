48eecf2a4d8f2557f3ffaa016f7dbef672ffa9ef
TFuray <pk.master881@gmail.com>
2022-11-29 16:31:44 -0800

    solved kata

--- 54-stringArrayDuplicates.js
+++ 54-stringArrayDuplicates.js
@@ -19,18 +19,28 @@
 let test = ["abbbracadabra","allottee","assessee"]
 
 
-function dup (s) {
-  let newArr = []
-  let final = []
-  s.forEach(el => {
-    for(let i=0;i<el.length;i++){
-        if(el[i] !== el[i+1]){
-        newArr.push(el[i])
-        } 
-    }
-    final.push(newArr.join(','))
-  });
-  return final
+// function dup (s) {
+//   let newArr = []
+//   let final = []
+//   s.forEach(el => {
+//     for(let i=0;i<el.length;i++){
+//         if(el[i] !== el[i+1]){
+//         newArr.push(el[i])
+//         } 
+//     }
+//     final.push(newArr.join(','))
+//   });
+//   return final
+// }
+
+function removeDupes (arr) {
+  //map -> word - split - filter - join
+  return arr.map(word =>
+    word
+      .split('')
+      .filter((l, i, a) => l != a[i - 1])
+      .join('')
+  )
 }
 
 
