9f0de61c3631f2b4582150d8a75a8fc470e7b857
TFuray <pk.master881@gmail.com>
2022-12-05 16:39:02 -0800

    solved 6kyu kata

---
+++ 54-countCharatersInString.js
@@ -0,0 +1,19 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+
+
+function count (str) {
+  //create obj
+  let countObj = {}
+  // loop -> check val exist ? val++ : val-1
+  for(const c of str){
+    if( countObj[c]){
+      countObj[c]++ 
+    }else{
+      countObj[c] = 1
+    }
+  }
+  return {}
+}
