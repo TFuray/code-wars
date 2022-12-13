df5db048b93a08433802bf8037407b76151e31d5
TFuray <pk.master881@gmail.com>
2022-12-13 14:52:05 -0800

    added leetcode problem

--- leetCode/4-containsDuplicate.js
+++ leetCode/4-containsDuplicate.js
@@ -26,4 +26,18 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function (nums) {}
+var containsDuplicate = function (nums) {
+  let numsMap = {}
+  for(let i=0; i< nums.length; i++){
+    let num = nums[i]
+    if(numsMap[num]){
+      return true
+    }else{
+      numsMap[num] = true
+    }
+  }
+  return false
+}
+
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+))
