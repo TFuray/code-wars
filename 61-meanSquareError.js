35cf51cac1ba37938f40b88a01e7fa79846c9e14
TFuray <pk.master881@gmail.com>
2023-01-11 22:44:42 -0800

    added test cases

--- 61-meanSquareError.js
+++ 61-meanSquareError.js
@@ -18,6 +18,6 @@ var solution = function (firstArray, secondArray) {
 }
 
 
-console.log(solution([1, 2, 3], [4, 5, 6]), 9)
-console.log(solution([10, 20, 10, 2], [10, 25, 5, -2]), 16.5)
-console.log(solution([-1, 0], [0, -1]), 1)
+// console.log(solution([1, 2, 3], [4, 5, 6]), 9)
+// console.log(solution([10, 20, 10, 2], [10, 25, 5, -2]), 16.5)
+// console.log(solution([-1, 0], [0, -1]), 1)
