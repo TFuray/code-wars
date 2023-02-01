c5e3afac6e28b0cd91bd842d3bfc96b13ac9561b
TFuray <pk.master881@gmail.com>
2023-02-01 15:14:11 -0800

    added completed recursive solution

--- 63-greatestCommonDivisor.js
+++ 63-greatestCommonDivisor.js
@@ -2,19 +2,22 @@
 
 // The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1. -->
 
-
 // The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
 
+// function mygcd (x, y) {
+//   for(let i = Math.min(x, y); i > 0; i--){
+//     if( x % i === 0 && y % i === 0 ) {
+//       return i
+//     }
+//   }
+// }
 
-
-function mygcd (x, y) {
-  for(let i = Math.min(x, y); i > 0; i--){
-    if( x % i === 0 && y % i === 0 ) {
-      return i
-    }
+function mygcd (a, b) {
+  if (b === 0) {
+    return a
   }
+  return mygcd(b, a % b)
 }
 
-
-console.log(mygcd(30,12), 6)
-console.log(mygcd(8,9),1);
+console.log(mygcd(30, 12), 6)
+console.log(mygcd(8, 9), 1)
