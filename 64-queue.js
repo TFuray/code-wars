89061fd2443f277646f67648848a6853065aea39
TFuray <pk.master881@gmail.com>
2023-02-06 15:18:47 -0800

    added queue class

--- 64-queue.js
+++ 64-queue.js
@@ -1,17 +1,38 @@
 class Queue {
   constructor(){
-    this.list = []
+    this.storage = {}
+    this.head = 0
+    this.tail = 0
   }
 
   size() {
     return this.list.length
   }
 
-  enqueue(item){
-    this.list.push(item)
+  enqueue(element){
+    this.storage[this.tail] = element
+    this.tail++
   }
 
   dequeue() {
-    return this.list.shift()
+    let removed = this.storage[this.head]
+    delete this.storage[this.head]
+    this.head++
+    return removed
   }
 }
+
+const queue = new Queue()
+
+queue.enqueue('apple')
+queue.enqueue('banana')
+queue.enqueue('pineapple')
+
+console.log(queue)
+
+queue.dequeue()
+console.log(queue)
+
+queue.enqueue('strawberry')
+
+console.log(queue)
