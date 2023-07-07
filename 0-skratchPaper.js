84fa29d7f7b1072a6c6d16a7a8407ec6845db7b9
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-07-06 23:42:04 -0700

    added leet code

--- 0-skratchPaper.js
+++ 0-skratchPaper.js
@@ -1,16 +1,36 @@
-const string = 'abcdddefghi'
+// const string = 'abcdddefghi'
 
-// return longest consecutive substring without repeats
+// // return longest consecutive substring without repeats
 
-const subString = (str) => {
-    let consecutive = []
-  const array = str.split('').map((val, index, array) => {
-    if (index > 0 && val !== array[index - 1] && val !== array[index + 1]) {
-      consecutive.push(val)
-    }
-  })
-  
-  return consecutive
+// const subString = (str) => {
+//     let consecutive = []
+//   const array = str.split('').map((val, index, array) => {
+//     if (index > 0 && val !== array[index - 1] && val !== array[index + 1]) {
+//       consecutive.push(val)
+//     }
+//   })
+
+//   return consecutive
+// }
+
+// console.log(subString(string))
+
+
+// -----------------------------------------------------------------------------------------------------
+
+
+// In: JPMORGANISHIRING, 3
+
+// J   R   I   R
+// P O G N S I I G
+// M   A   H   N
+
+// out: JRIRPOGNSIIGMAHN
+
+// loop through string
+// assign array for each row
+//based on row number follow pattern assigning elements to each row array ex: rows=3 1,2,3,2,1,2,3,
+
+const zigZag = (string, rows) => {
+
 }
-
-console.log(subString(string))
---
+++ leetCode/9-containsDuplicatesPt2.js
@@ -0,0 +1,9 @@
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+
+};
