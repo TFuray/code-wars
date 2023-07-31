40f28ef23c7cbe2e505777ffdf3992d77c6bd45e
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-07-31 12:39:25 -0700

    chatGPT generated question

--- leetCode/93-sumPairs.js
+++ leetCode/93-sumPairs.js
@@ -31,6 +31,38 @@
 // The targetSum will be an integer between -2000 and 2000 (inclusive).
 // Your task is to implement the findSumPairs function. Ensure your solution is efficient and handles edge cases appropriately. You may assume that the input array will always have at least one valid pair that sums up to the targetSum.
 
-const findSumPairs = (arr, targetSum) => {
-  
+function findSumPairsOptimized(arr, targetSum) {
+  const result = []
+  const numMap = {}
+
+  for (let i = 0; i < arr.length; i++) {
+    const complement = targetSum - arr[i]
+    if (numMap[complement] !== undefined) {
+      result.push([complement, arr[i]].sort((a, b) => a - b))
+    }
+    numMap[arr[i]] = i
+  }
+
+  return result
+}
+
+// console.log(findSumPairsOptimized([3,4,5,2,3,1,3,4,4,434,34,1,2,21,1], 8))
+
+
+const testCases = [
+  { arr: [1, 2, 3, 4, 5], targetSum: 7 },
+  { arr: [3, 9, 2, 1, 5], targetSum: 10 },
+  { arr: [6, 2, 4, 8, 2, 5], targetSum: 10 },
+  { arr: [1, 2, 3, 4, 5], targetSum: 8 },
+  // Add more test cases if desired
+]
+
+console.log("\nOptimized Method:")
+for (const testCase of testCases) {
+  const result = findSumPairsOptimized(testCase.arr, testCase.targetSum)
+  console.log(
+    `Input: ${testCase.arr}, Target Sum: ${
+      testCase.targetSum
+    } -> Output: ${JSON.stringify(result)}`
+  )
 }
