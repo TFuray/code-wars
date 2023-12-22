8f9fb52324e90bfb7fd38bd1b088234d16fd5e61
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-12-21 23:48:12 -0800

    leetcode strings problem solved

--- leetCode/106-firstUniqueChar.js
+++ leetCode/106-firstUniqueChar.js
@@ -1,7 +1,5 @@
 // Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
-
-
 // Example 1:
 
 // Input: s = "leetcode"
@@ -15,13 +13,32 @@
 // Input: s = "aabb"
 // Output: -1
 
-
 // Constraints:
 
 // 1 <= s.length <= 105
 // s consists of only lowercase English letters.
 
 var firstUniqChar = function (s) {
-
-  
+  let charMap = {}
+  let singles = []
+  let pos = []
+  for (const el of s) {
+    charMap[el] = charMap[el] + 1 || 1
+  }
+  for (const el in charMap) {
+    if (charMap[el] === 1) {
+      singles.push(el)
+    }
+  }
+  for (const el in s) {
+    if (singles.includes(s[el])) {
+      pos.push(el)
+    }
+  }
+  if (pos[0] === undefined) {
+    return -1
+  } else return pos[0]
 }
+
+// console.log(firstUniqChar('loveleetcode'))
+console.log(firstUniqChar("aabb"))
