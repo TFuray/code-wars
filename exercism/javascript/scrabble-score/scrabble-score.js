9825ac85b323f01324dac3ccf1285315dd23cfee
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2024-01-21 14:24:12 -0800

    scrabble-score complete

---
+++ exercism/javascript/scrabble-score/scrabble-score.js
@@ -0,0 +1,42 @@
+//
+// This is only a SKELETON file for the 'Scrabble Score' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const score = (word) => {
+  const letterValues = {
+    a: 1,
+    b: 3,
+    c: 3,
+    d: 2,
+    e: 1,
+    f: 4,
+    g: 2,
+    h: 4,
+    i: 1,
+    j: 8,
+    k: 5,
+    l: 1,
+    m: 3,
+    n: 1,
+    o: 1,
+    p: 3,
+    q: 10,
+    r: 1,
+    s: 1,
+    t: 1,
+    u: 1,
+    v: 4,
+    w: 4,
+    x: 8,
+    y: 4,
+    z: 10,
+  }
+  let total = 0
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i].toLowerCase()
+    total += letterValues[letter] || 0
+  }
+  return total
+}
