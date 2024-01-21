bfa17415ce925c82614f58c77c786f77dbab1386
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2024-01-20 23:13:28 -0800

    word count start

--- exercism/javascript/word-count/word-count.js
+++ exercism/javascript/word-count/word-count.js
@@ -2,11 +2,19 @@
 // This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+let str =
+  "That's the password: 'PASSWORD 123'!, cried the Special Agent.\nSo I fled."
+export const countWords = (str) => {
+const regexPattern = /\b(?:[^\w']+'\w+|[^\w'\s])*\b/g
 
-export const countWords = () => {
-  let str = "That's the password: 'PASSWORD 123'!, cried the Special Agent.\nSo I fled."
-const result = str.split(/[,.\s]/)
-console.log(result)
-};
+  const result = str.toLowerCase().split(regexPattern)
+  console.log(str.toLowerCase())
+  let charMap = {}
 
-console.log(countWords())
+  for (word of result) {
+    charMap[word] = charMap[word] + 1 || 1
+  }
+  console.log(charMap)
+}
+
+console.log(countWords(str))
