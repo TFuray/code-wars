c59ec50e8bc785f5aec1b6d08f063ac82008a5e4
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2024-01-24 17:41:20 -0800

    not above one kata init

---
+++ 7 kyu/111-notAboveTheOne.js
@@ -0,0 +1,22 @@
+// Task
+// Implement a function which finds the numbers less than 2, and the indices of numbers greater than 1 in the given sequence, and returns them as a pair of sequences.
+
+// Return a nested array or a tuple depending on the language:
+
+// The first sequence being only the 1s and 0s from the original sequence.
+// The second sequence being the indexes of the elements greater than 1 in the original sequence.
+// Examples
+// [ 0, 1, 2, 1, 5, 6, 2, 1, 1, 0 ] => [ [ 0, 1, 1, 1, 1, 0 ], [ 2, 4, 5, 6 ] ]
+// Please upvote and enjoy!
+
+
+function binaryCleaner(arr) {
+  //
+}
+
+
+
+console.log(binaryCleaner([0,1,2,1,0,2,1,1,1,0,4,5,6,2,1,1,0]), [ [ 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0 ], [ 2, 5, 10, 11, 12, 13 ] ], "Should return")
+// console.log(binaryCleaner([0,1,1,2,0]), [ [ 0, 1, 1, 0 ], [ 3 ] ], "Should return")
+// console.log(binaryCleaner([2,2,0]), [ [ 0 ], [ 0, 1 ] ], "Should return: ")
+// console.log(binaryCleaner([0,1,2,1,0,2,1,1]), [ [ 0, 1, 1, 0, 1, 1 ], [ 2, 5 ] ], "Should return")
