3330a512033b55b7d3d6ccd004c0947dd8b83cb0
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2024-02-05 16:13:29 -0800

    completed notAboveTheOne

--- 7 kyu/111-notAboveTheOne.js
+++ 7 kyu/111-notAboveTheOne.js
@@ -9,15 +9,28 @@
 // [ 0, 1, 2, 1, 5, 6, 2, 1, 1, 0 ] => [ [ 0, 1, 1, 1, 1, 0 ], [ 2, 4, 5, 6 ] ]
 // Please upvote and enjoy!
 
-
 function binaryCleaner(arr) {
+  let lessThan = []
+  let greaterThan = []
 
-  
+  arr.map((value, index) => {
+    if (value < 2) {
+      lessThan.push(value)
+    } else {
+      greaterThan.push(index)
+    }
+  })
+  return [lessThan, greaterThan]
 }
 
-
-
-console.log(binaryCleaner([0,1,2,1,0,2,1,1,1,0,4,5,6,2,1,1,0]), [ [ 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0 ], [ 2, 5, 10, 11, 12, 13 ] ], "Should return")
+console.log(
+  binaryCleaner([0, 1, 2, 1, 0, 2, 1, 1, 1, 0, 4, 5, 6, 2, 1, 1, 0]),
+  [
+    [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0],
+    [2, 5, 10, 11, 12, 13],
+  ],
+  "Should return"
+)
 // console.log(binaryCleaner([0,1,1,2,0]), [ [ 0, 1, 1, 0 ], [ 3 ] ], "Should return")
 // console.log(binaryCleaner([2,2,0]), [ [ 0 ], [ 0, 1 ] ], "Should return: ")
 // console.log(binaryCleaner([0,1,2,1,0,2,1,1]), [ [ 0, 1, 1, 0, 1, 1 ], [ 2, 5 ] ], "Should return")
