e325918bdc328f17b0097c2179f9c26b6afedcf7
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2024-04-01 13:20:51 -0700

    number format

---
+++ 6 kyu/116-numberFormat.js
@@ -0,0 +1,14 @@
+// Format any integer provided into a string with "," (commas) in the correct places.
+
+// Example:
+
+// For n = 100000 the function should return '100,000';
+// For n = 5678545 the function should return '5,678,545';
+// for n = -420902 the function should return '-420,902';
+
+var numberFormat = function (number) {
+  //Your code here
+}
+
+console.log(numberFormat(100000))
+console.log(numberFormat(5678545))
