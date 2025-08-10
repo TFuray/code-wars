c9f38806d8bbc62e8845f8ae54430e498231f948
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2025-08-10 15:29:33 -0700

    highest profit wins

---
+++ 7 kyu/highestProfitWins.js
@@ -0,0 +1,26 @@
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// Examples (Input --> Output)
+// [1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+// Remarks
+// All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+function minMax(arr) {
+  let min = arr[0]
+  let max = arr[0]
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+  }
+  return [min, max]
+}
+
+console.log(minMax([ 4, 5, 32, 34, 5, 121, 94]))
--- 7 kyu/tempCodeRunnerFile.js
+++ 7 kyu/tempCodeRunnerFile.js
@@ -1,3 +1,14 @@
-  if (val <= ary[0]) {
-    return 0
-  }
+function minMax(arr) {
+//   let min = arr[0]
+//   let max = arr[0]
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr.i < min) {
+//       min = arr[i]
+//     }
+//     if (arr.i > max) {
+//       max = arr[i]
+//     }
+//   }
+//   return [min, max]
+// }
