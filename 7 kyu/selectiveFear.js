3b74c83164969283926b36c45640a79d24253fdb
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2025-08-15 12:21:59 -0700

    selective fear

--- 7 kyu/selectiveFear.js
+++ 7 kyu/selectiveFear.js
@@ -24,4 +24,23 @@ var AmIAfraid = function (day, num) {
   if (day == 'Tuesday' && num > 95) {
     return true
   }
+  if (day == 'Wednesday' && num == 34) {
+    return true
+  }
+  if (day == 'Thursday' && num == 0) {
+    return true
+  }
+  if (day == 'Friday' && num % 2 == 0) {
+    return true
+  }
+  if (day == 'Saturday' && num == 56) {
+    return true
+  }
+  if (day == 'Sunday' && num == 666 | num == -666) {
+    return true
+  }
+
+  else return false
 }
+
+console.log(AmIAfraid('Sunday', -666))
